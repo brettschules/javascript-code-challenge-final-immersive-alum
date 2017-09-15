95c4bf8de6fd78630a1b6f0493b834549eb26e57
Brett Schules <brettschules@gmail.com>
2017-09-14 20:28:08 -0400

    getting input of image info from form and saving into array

--- src/index.js
+++ src/index.js
@@ -4,7 +4,52 @@
 //   )
 // })
 
-var addNewPictureButton = document.getElementById("add-new-picture-button")
-var newImageModel = document.getElementById("new-image")
 
-addNewPictureButton.addEventListener("click", () => newImageModel.style.display = 'block')
+function pictureModel() {
+  var addNewPictureButton = document.getElementById("add-new-picture-button")
+  var newImageModel = document.getElementById("new-image")
+
+  function showPictureModelAndTriggerSumbitPicture() {
+    newImageModel.style.display = 'block'
+    submitPicture()
+  }
+
+  addNewPictureButton.addEventListener("click", showPictureModelAndTriggerSumbitPicture)
+  submitPicture
+}
+
+function submitPicture() {
+
+  var imgInfo = [["http://cdn.skim.gs/images/homer-simpson-doughnuts/what-homer-simpson-taught-us-about-doughnuts", "doh"]]
+  var submitPictureClick = document.getElementById("new-image-submit-button")
+
+  submitPictureClick.addEventListener("click", storeImgInfo)
+  // submitPictureClick.addEventListener("click", () => )
+}
+
+function diplayPictures() {
+
+
+}
+
+function getCaptionInfo() {
+  return document.getElementById("caption").value;
+}
+
+function getImgInfo() {
+  return document.getElementById("image-url").value;
+}
+
+function combineTheTwo() {
+  return [getImgInfo(), getCaptionInfo()]
+}
+
+function storeImgInfo(event) {
+  event.preventDefault()
+  var imgInfo = []
+
+  imgInfo.push(combineTheTwo())
+  debugger
+}
+
+pictureModel()
